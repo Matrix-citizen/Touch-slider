235be36be0b4846ba869cfcd7521911e90834c10
Rostyslav <rklyba@yahoo.com>
2021-02-04 09:28:11 +0200

    Added function to disable context menu

--- script.js
+++ script.js
@@ -11,4 +11,26 @@ let currentIndex = 0;
 slides.forEach((slide, index) => {
   const slideImage = slide.querySelector('img');
   slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+
+  // Touch events
+  slide.addEventListener('touchstart', touchStart(index))
+  slide.addEventListener('touchend', touchEnd);
+  slide.addEventListener('touchmove', touchMove);
+
+  // Mouse events
+  slide.addEventListener('mousedown', touchStart(index));
+  slide.addEventListener('mouseup', touchEnd);
+  slide.addEventListener('mouseleave', touchEnd);
+  slide.addEventListener('mousemove', touchMove);
 })
+
+// Disable context menu
+window.oncontextmenu = function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+} 
+
+
+
+
